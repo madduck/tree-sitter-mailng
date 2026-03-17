27257eb596cac09cb7e01ef4b89fbc424faa6804
martin f. krafft <madduck@madduck.net>
2026-03-17 21:28:01 +0100

    Add Date header handling

--- grammar.js
+++ grammar.js
@@ -27,6 +27,8 @@ export default grammar({
         )
       ),
 
+    _ws_no_nl: _$ => /[ \t]+/,
+
     // Headers is a collection of one or more _header instances
     headers: $ => repeat1($._header),
 
@@ -43,6 +45,7 @@ export default grammar({
     // queries later e.g. for syntax highlighting:
     _header: $ => seq(
       choice(
+        $.header_date,
         $.header_from,
         $.header_email,
         $.header_subject,
@@ -59,6 +62,23 @@ export default grammar({
 
     // The From header just has one correspondent (TODO: could actually have more, and should be fused)
     header_from: $ => seq("From", $._header_separator, $.correspondent),
+
+    // The date header uses a standard RFC5322 format
+    _date_dow: _$ => /(Mon|Tue|Wed|Thu|Fri|Sat|Sun)/,
+    _date_day: _$ => /\d{1,2}/,
+    _date_month: _$ => /(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/,
+    _date_year: _$ => /\d{4}/,
+    _date_time: _$ => /\d{2}:\d{2}:\d{2}/,
+    _date_tzoffset: _$ => /[-+]\d{4}/,
+    date: $ => seq(
+      $._date_dow, ",", repeat1($._ws_no_nl),
+      $._date_day, repeat1($._ws_no_nl),
+      $._date_month, repeat1($._ws_no_nl),
+      $._date_year, repeat1($._ws_no_nl),
+      $._date_time, repeat1($._ws_no_nl),
+      choice("GMT", $._date_tzoffset)
+    ),
+    header_date: $ => seq("Date", $._header_separator, $.date),
 
     // Other email headers take multiple correspondents. For now, lines can split only
     // after the comma used to separate multiple correspondents. TODO: lines can break elsewhere too…
