6bb281e36a4a6c32e31e0854784fb5fcec85695c
martin f. krafft <madduck@madduck.net>
2026-03-17 21:09:18 +0100

    Simplification thanks to input by BubbleFish on Discord

--- grammar.js
+++ grammar.js
@@ -7,24 +7,6 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-const NEWLINE = /\r?\n/;
-const WS_NO_NL = /[ \t]/;
-const HEADER_FIELD_SEP = new RegExp(":" + WS_NO_NL.source + "*");
-const NEWLINE_CONTINUED = new RegExp("(?:" + NEWLINE.source + "[ \t]+)*");
-const MULTILINE_HEADER = new RegExp("(?:.*|.+" + NEWLINE_CONTINUED.source + ".+)");
-
-function separated_list(item, sep = /,/) {
-  return seq(item, repeat(seq(sep, item)))
-}
-
-function header(label, contents) {
-  return seq(
-    label,
-    HEADER_FIELD_SEP,
-    contents,
-  )
-}
-
 export default grammar({
   name: "mail",
 
@@ -44,9 +26,9 @@ export default grammar({
       ),
 
     headers: $ => repeat1($._header),
-    body_separator: _$ => NEWLINE,
-    body: $ => repeat1($.block),
-    block: _$ => /.+/,
+    body_separator: _$ => /\r?\n/,
+    body: $ => repeat1($._block),
+    _block: _$ => /.+\r?\n/,
 
     _header: $ => seq(
       choice(
@@ -54,38 +36,28 @@ export default grammar({
         $.header_email,
         $.header_subject,
         $.header_other
-      ), NEWLINE
+      ), /\r?\n/
     ),
 
     correspondent: _$ => token(/[a-z]+/),
+    _header_label: _$ => /[-\w]+/,
+    _header_separator: _$ => /:[ \t]*/,
 
-    _fieldname_from: _$ => 'From',
-    header_from: $ => header($._fieldname_from, $.correspondent),
 
-    _fieldname_email: _$ => choice('To', 'Cc', 'Bcc', 'Reply-To'),
-    header_email: $ => header(
-      $._fieldname_email,
-      separated_list(
-        $.correspondent,
-        new RegExp("," + WS_NO_NL.source + "*" + NEWLINE_CONTINUED.source)
-      )
     ),
 
-    multiline_contents: _$ => token(MULTILINE_HEADER),
+    multiline_contents: _$ => /.+(\r?\n[ \t]+.+)*/,
 
-    _fieldname_subject: _$ => "Subject",
-    header_subject: $ => header(
-      $._fieldname_subject,
-      alias($.multiline_contents, $.subject)
+    header_from: $ => seq("From", $._header_separator, $.correspondent),
+
+    _comma_continued: _$ => /,[ \t]*(?:\r?\n[ \t]+)?/,
+    header_email: $ => seq(
+      choice('To', 'Cc', 'Bcc', 'Reply-To'),
+      $._header_separator,
+      seq($.correspondent, repeat(seq($._comma_continued, optional($.correspondent))))
     ),
-    // subject: _$ => token(MULTILINE_HEADER),
 
-    _fieldname_other: _$ => token(/[-\w]+/),
-    header_other: $ => header(
-      $._fieldname_other,
-      alias($.multiline_contents, $.contents)
-    ),
-    // other_contents: _$ => token(MULTILINE_HEADER),
-
+    header_subject: $ => seq("Subject", $._header_separator, alias($.multiline_contents, $.subject)),
+    header_other: $ => seq($._header_label, $._header_separator, alias($.multiline_contents, $.contents)),
   }
 });
